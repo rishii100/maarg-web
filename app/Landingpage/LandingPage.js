08907dfd05622d9e5b0048f71c02de4e17229925
Ishaan Khullar <ishaan.khullar06@gmail.com>
2024-02-07 16:52:03 +0530

    Added landing page title and colors!

---
+++ app/Landingpage/LandingPage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div className="w-full h-full flex flex-row">
+      <div>
+        <h1 className="text-2xl font-bold text-black font-inter">MAARG</h1>
+        <p className="text-[16vw] md:text-[130px] font-extrabold text-black leading-[0.9] font-inter">
+            Find your Way to
+            <br/>
+            the <span className="text-[#407BBF] font-inter">Destination.</span>
+        </p>
+      </div>
+
+       {/* Animation */}
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
